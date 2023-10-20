a12a7375d7d72f7cd233b3c9860b270058a82d44
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-20 18:26:41 +0530

    Environment setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <p className="text-red-500 font-bold">Hello Notion Clone</p>
+      <Button variant="purple" size="lg">
+        Click Me
+      </Button>
+    </>
+  );
+}
